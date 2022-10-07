fe78cdc4c18d693f915aed94f35069817f6aac48
49_CYS_John <johnbabyeldho@gmail.com>
2022-10-08 00:57:47 +0530

    Vul scan cve:

--- portvul.py
+++ portvul.py
@@ -6,6 +6,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 x = s.getsockname()[0]
 s.close()
-subprocess.run(f"nmap --script nmap-vulners/,vulscan/ --script-args vulscandb=scipvuldb.csv -sV {x} -p{port}")
-with open("output.txt", "r") as file:
+subprocess.run(f"nmap --script nmap-vulners/,vulscan/ --script-args vulscandb=scipvuldb.csv -sV {x} -p{port} -oN output1.txt")
+with open("output1.txt", "r") as file:
     k = file.read()
