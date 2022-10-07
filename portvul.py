5041a1e949abf4addf489f9c623babd0a71cad4a
49_CYS_John <johnbabyeldho@gmail.com>
2022-10-08 01:05:54 +0530

    port vul scan

--- portvul.py
+++ portvul.py
@@ -1,11 +1,10 @@
 import socket
 import subprocess
-import re
-
+import os
+port = 5432
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 x = s.getsockname()[0]
 s.close()
 subprocess.run(f"nmap --script nmap-vulners/,vulscan/ --script-args vulscandb=scipvuldb.csv -sV {x} -p{port} -oN output1.txt")
-with open("output1.txt", "r") as file:
-    k = file.read()
+os.system("output1.txt")
